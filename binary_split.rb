72f56f532710b34d2d0efae43842180373e48a38
Thomas Harrold <gomtom@users.noreply.github.com>
2016-06-12 14:18:24 +1000

    Better solution for binary split

--- binary_split.rb
+++ binary_split.rb
@@ -1,19 +1,15 @@
-
-
+# you can write to stdout for debugging purposes, e.g.
+# puts "this is a debug message"
 
 def solution(n)
   # write your code in Ruby 2.2
-
-  puts n
-
-  binary = n.to_s(2)
-
-  puts binary
-
-  zeroes = binary.split('1')
-
-  puts zeroes
-
+  
+  # convert the integer to binary a string and remove any zeros not surrounded by 1's
+  binary = n.to_s(2).slice(/1.*1/)
+  
+  # split the binary gaps
+  zeros = binary.split(/1/)
+    
+  # no binary gaps? return 0 otherwise return the largest gap    
+  zeros.empty? ? (return 0) : (return zeros.map { |zeros| zeros.length }.max)
 end
-
-solution(1041)
