d9a46293e04d3cbe4ca98137ce99c6c4aa196956
Thomas Harrold <tharrold@internode.on.net>
2016-06-14 18:42:09 +1000

    Solution for missing integer

--- missing_integer.rb
+++ missing_integer.rb
@@ -1,23 +1,44 @@
+# # you can write to stdout for debugging purposes, e.g.
+# # puts "this is a debug message"
+#
+# def solution(a)
+#   # write your code in Ruby 2.2
+#
+#   max = a.max
+#
+#   if max <= 0
+#     return 1
+#
+#   else
+#
+#     (1...max).each do |num|
+#
+#       return num if !a.include?(num)
+#
+#     end
+#
+#   end
+#
+#
+# end
+
+
 # you can write to stdout for debugging purposes, e.g.
 # puts "this is a debug message"
 
 def solution(a)
   # write your code in Ruby 2.2
 
-  max = a.max
+  n = a.size
 
-  if max <= 0
-    return 1
+  return 1 if n == 1
 
-  else
+  # remove duplicates
+  uniques = a.uniq
 
-    (1...max).each do |num|
-
-      return num if !a.include?(num)
-
-    end
-
+  # since we are looking for the minimal positive integer lets iterate through and check if a includes each vaule
+  (1...n).each do |element|
+    return element if !a.include?(element)
   end
-
 
 end
